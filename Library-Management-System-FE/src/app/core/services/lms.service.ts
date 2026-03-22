73a9f9cf8b5fc3365dcf5e855b6b61eff4e9d42c
Antonius Atef Ghaly <aaghaly.vpn>
2026-03-22 20:37:04 +0300

    feat: Initialize Angular project with core API service and environment configurations.

--- Library-Management-System-FE/src/app/core/services/lms.service.ts
+++ Library-Management-System-FE/src/app/core/services/lms.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ApiResult, Author, AuthorParams,BookParams,Book } from '../models/lms.models';
+import { ApiResult, Author, AuthorParams, BookParams, Book } from '../models/lms.models';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
+
   providedIn: 'root'
 })
 export class LmsService {
-  private apiUrl = 'https://library-management-api-h3en.onrender.com/api'; // Live Render backend
+  private apiUrl = environment.apiUrl;
+
 
   constructor(private http: HttpClient) { }
 
---
+++ Library-Management-System-FE/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://library-management-api-h3en.onrender.com/api'
+};
